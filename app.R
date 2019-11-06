57fec04a79ec2325158b06b1e3fa4c6ee2b19e5d
mmingay2 <mmingay2@gmail.com>
2019-11-05 18:50:47 -0800

    update app; subset data

--- app.R
+++ app.R
@@ -8,89 +8,91 @@ library(reshape2)
 library(data.table)
 library(dplyr)
 
-commonTheme = list(labs(color="Density",fill="Density"),
-                   theme_bw(),
-                   theme(legend.position=c(0,1),
-                         legend.justification=c(0,1)))
-
-#tcga_rna <- read.delim("~/Google Drive/epigenomics_drive/tcga/laml_tcga_pub/laml_tcga_pub/data_RNA_Seq_v2_expression_median.txt")
+#load datasets
 rnamelt <- data.frame(fread("./rnamelt.txt"))
 tcga_mut <- data.frame(fread("./tcga_mut.txt"))
 
-# Define UI for application that draws a histogram
-ui <- navbarPage(title="TCGA RNA-seq Explorer", theme = shinythemes::shinytheme("sandstone"),
-                 tabPanel(title = "TCGA Genes of Interest vs. Mutational Status",
-                          fluidPage(
-                            # Sidebar with controls to select the variable to plot against mpg
-                            # and to specify whether outliers should be included
-                            sidebarPanel(
-                              selectInput("muts", "Separate by Mutational Status", choices = as.character(unique(tcga_mut$mutation)), multiple = T, selected = as.character(unique(tcga_mut$mutation))[c(4,50,258)]),
-                              selectInput("geneselect", "Select Genes to Plot", choices = as.character(unique(rnamelt$gene)), multiple = T, selected = c("HOXA9", "CSF2RA", "KIT")),
-                              tableOutput("presult")
-                              #tableOutput("pvals")
-                              #selectInput("fillm", "Colour genes in vitaminC region:", names(data), multiple = F, selected = "dxmr_2kbTSS"),
-                            ),
-                            # Show the caption and plot of the requested variable against mpg
-                            mainPanel(
-                              plotOutput("mutplot", 
-                                         width = "100%",  height = "100%",
-                                         hover = "plot_hover",
-                                         click = "plot_click")
-                            )
-                          ))
-)
+# Define UI for application that will displays options, plots and interact with our users
+ui <- 
+  ##Use fluid page to create a page and hold our sidebarPanel and mainPanel
+  fluidPage(#give the page a title
+                title = "TCGA Genes of Interest vs. Mutational Status",
+                #lets apply a theme using the 'shinythemes' package we loaded'
+                theme = shinythemes::shinytheme("sandstone"),
+                #hold our input selection boxes within a "sidebarPanel" within our FluidPage
+                        sidebarPanel(
+                              #Define a select box for mutation status
+                                selectInput(
+                                      #our inputId will be how we access the value selected.
+                                      #it is accessed using `input$muts` or more generally `input$inputId``
+                                      inputId = "muts", label = "Separate by Mutational Status", #text to display above the box
+                                      #this parameter is where we define the choices available          
+                                      choices = as.character(unique(tcga_mut$mutation)), 
+                                      #can the user select multiple options?
+                                      multiple = TRUE, 
+                                      #what will be selected by default?
+                                      selected = as.character(unique(tcga_mut$mutation))[c(4,50,258)]),
+                                #Same as above but assigning selected genes to `input$geneselect`
+                                selectInput(inputId = "geneselect", label = "Select Genes to Plot", choices = as.character(unique(rnamelt$gene)), multiple = T, selected = c("HOXA9", "CSF2RA", "KIT")),
+                                #This function will show a nicely table defined in the server function as output$presult
+                                tableOutput("presult")
+                      ),#End of SidebarPanel
+              
+                      # Create a mainPanel to display the dynamic plot
+                        mainPanel(
+                                #this function is needed to display plots created in the server function
+                                plotOutput(
+                                  # display the plot called `output$mutplot` defined in the server function
+                                  outputId = "mutplot", 
+                                           width = "100%", height = "100%",
+                                           #creates `input$plot_hover` and `input$plot_click` variables depending on a user's cursor activity
+                                           hover = "plot_hover", click = "plot_click")
+                        ) #end of mainPanel
+              )#end of FluidPage and UI element
 
+# Define server logic required to apply user input to modify plots as `server`
 
-# Define server logic required to draw a histogram
 server <- shinyServer(function(input, output) {
   
-  mutreact <- reactive({
-    nmut <- tbl_df(tcga_mut) %>% select(-aa_change) %>% unique() %>% filter(mutation %in% input$muts) %>% group_by(id) %>% tally()
-    tbl_df(tcga_mut) %>%
-      filter(mutation %in% input$muts) %>% 
-      inner_join(nmut) %>% 
-      mutate(status=ifelse(n < 2, as.character(mutation), "multiple")) %>% 
-      select(id, status) %>% 
-      unique() %>%
-      inner_join(rnamelt) %>% filter(gene %in% input$geneselect)
+  # this reactive function outputs our counts (from rnamelt) filtered for genes select by user (input$geneselect)
+  # ({ ... }) is the syntax used to define reactive functions. 
+  # this data here can be accessed using `genereact()`
+  genereact <- reactive({
+    filter(rnamelt, gene %in% input$geneselect) %>% 
+      # create a new column called `logtag` for log(1+count)
+      mutate(logtag=log(1+tagcount))
   })
-  
-#  muts <- c("IDH1", "IDH2")
-#  geneselect <- c("HOXA9", "CSF2RA", "PDGFRB")
-  mutreactb <- reactive({
-     # nmut <-  tbl_df(tcga_mut) %>% select(-aa_change) %>% unique() %>% filter(mutation %in% muts) %>% group_by(id) %>% tally()
-    #nmut <- tbl_df(tcga_mut) %>% select(-aa_change) %>% unique() %>% mutate(mutant=ifelse(mutation %in% input$muts, yes = mutation, no = "other")) %>% select(mutant, id) %>% unique()
-    nmut <- tbl_df(tcga_mut) %>% select(-aa_change) %>% unique() %>% filter(mutation %in% input$muts) %>% group_by(id) %>% tally()
-    mutid <- tbl_df(tcga_mut) %>%
-      filter(mutation %in% input$muts) %>% 
-      inner_join(nmut) %>% 
-      mutate(status=ifelse(n < 2, as.character(mutation), "multiple")) %>% 
-      select(id, status) %>% 
-      unique() %>% data.frame()
 
-    tbl_df(tcga_mut) %>% select(-aa_change, -mutation) %>% unique() %>% full_join(mutid, by="id") %>% 
-      mutate(mutant=ifelse(is.na(status), yes = "other", no = status)) %>% select(-status) %>%
-      select(mutant, id) %>%
-      inner_join(rnamelt) %>% filter(gene %in% input$geneselect) %>% mutate(logtag=log(1+tagcount))
-  })
-  
-  
-  output$infod <- renderDataTable({
-      scatreact() %>% tbl_df() %>% nearPoints(input$plot_click, maxpoints = 25, threshold = 10)
+  # identify patient id that have mutations selected by user; also see if they have >1 mutation (multiple)
+  mutid <-  reactive({
+    filter(tcga_mut, mutation %in% input$muts) %>% 
+    inner_join(nmut()) %>% 
+    mutate(status=ifelse(n < 2, as.character(mutation), "multiple")) %>% 
+    select(id, status) %>% 
+    unique() %>% data.frame()
     })
-    
-  output$infor <- renderDataTable({
-    mutreactb() %>% tbl_df() %>% nearPoints(input$plot_click, maxpoints = 25, threshold = 10)
+  # create a reactive function that detects changes in `input$muts` and `input$geneselect` and updates accordingly
+  mutreact <- reactive({
+      select(tcga_mut, -aa_change, -mutation) %>% unique() %>%
+      full_join(mutid(), by="id") %>%
+      # group patients by `status`...if they don't have one of selected mutations they are "other"
+      mutate(mutant=ifelse(is.na(status), yes = "other", no = status)) %>% 
+      select(mutant, id) %>%
+      # join with gene expression counts
+      inner_join(genereact())
   })
   
-  
-  
-  
+  # get the number of patients with the selected mutations (from input$muts)
+  nmut <- reactive({
+    filter(tcga_mut, mutation %in% input$muts) %>% group_by(id) %>% tally()
+  })
+
+  # create the plot using the reactive data
   output$mutplot <- renderPlot({
-    mutreactb() %>%
+    mutreact() %>%
         ggplot(aes(x=mutant, y=logtag))+
         geom_boxplot(position="dodge", width=0.7, fill="honeydew3", alpha=0.3, outlier.colour = "white")+
-        geom_point(data=mutreactb(), aes(x=mutant, y=logtag, colour=mutant), position="jitter", size=0.75)+
+        geom_point(data=mutreact(), aes(x=mutant, y=logtag, colour=mutant), position="jitter", size=0.75)+
         ylab("Expression log(1+tagcount)")+
         xlab("")+
       theme_bw()+
@@ -101,34 +103,14 @@ server <- shinyServer(function(input, output) {
     }, width = 900, height = 450)
   
   output$mutable <- renderDataTable({
-    # renderPrint({
-    # if(is.null(input$plot_hover)) return("hover over a point")
-    # With base graphics, need to tell it what the x and y variables are.
-    mutreactb() %>% group_by(gene, mutant) %>% summarize(meanlog1plus=mean(log(1+tagcount)), sdlog1plus=sd(log(1+tagcount)))
-    # nearPoints() also works with hover and dblclick events
-    #})
+    mutreact() %>% group_by(gene, mutant) %>% summarize(meanlog1plus=mean(log(1+tagcount)), sdlog1plus=sd(log(1+tagcount)))
   })
   
-  output$caption <- renderText({ "# of patients with mutations in:" })
-  
   output$presult <- renderTable({
-    print <- mutreactb() %>% select(id,mutant) %>% unique() %>% group_by(mutant) %>% tally() %>% data.frame()
+    print <- mutreact() %>% select(id,mutant) %>% unique() %>% group_by(mutant) %>% tally() %>% data.frame()
     colnames(print) <- c("Mutated Gene", "# of patients")
     print
   })
-  
-  
-  output$stats <- renderTable({
-  mutreact() %>%
-    select(-id) %>%
-    group_by(status, gene) %>% 
-    summarise(value = list(tagcount)) %>% 
-    spread(gene, value) %>% 
-    group_by(status) %>% 
-    mutate(p_value = t.test(unlist(input$ttesta), unlist(input$ttestb))$p.value,
-           t_value = t.test(unlist(input$ttesta), unlist(input$ttestb))$statistic)
-  })
-  
 })
 
 # Run the application 
